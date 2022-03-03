bd5866364be6fb4c00756dbf3923ca5b4f54e316
Mykiora <90429463+Mykiora@users.noreply.github.com>
2022-03-03 22:42:03 +0100

    Update script.js
    Correcting some plurals I got wrong in the comments

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,5 @@
 /**
- * Generate an array of characters from the unicode table in a given range
+ * Generates an array of characters from the unicode table in a given range
  * @param {number} start Where the range begins
  * @param {number} end Where the range ends
  * @returns {object} Array containing the characters
@@ -15,7 +15,7 @@ function generateCharacters(start, end) {
 }
 
 /**
- * Generate a random integer inside a given range
+ * Generates a random integer inside a given range
  * @param {number} min Minimum value -> default is 0
  * @param {number} max Maximum value -> default is 100
  * @returns {number}
@@ -60,7 +60,7 @@ function generatePassword() {
 }
 
 /**
- * Replace the default password field in the page with the generated password
+ * Replaces the default password field in the page with the generated password
  * @param {string} password The generated password 
  */
 modifyPasswordField = function (password) {
